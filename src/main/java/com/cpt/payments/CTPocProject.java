111113abef4146d7609006a1dc265b10329f6fef
sudheerpolibothu0706 <sudheerpolibothu@gmail.com>
2023-09-14 16:18:29 +0530

    initial draft of project

---
+++ src/main/java/com/cpt/payments/CTPocProject.java
@@ -0,0 +1,21 @@
+package com.cpt.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@SpringBootApplication(scanBasePackages = { "com.cpt.payments" })
+@EnableAsync
+@EnableScheduling
+public class CTPocProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTPocProject.class, args);
+		System.out.println("application started");
+		System.out.println("By sudher");
+	}
+
+}
